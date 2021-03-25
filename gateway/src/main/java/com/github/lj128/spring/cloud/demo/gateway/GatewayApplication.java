10d5c8c8c7ecda32381d928f9bf4186e1be69ecd
liujia <liujia@imdada.cn>
2021-03-25 15:16:13 +0800

    升级Spring Cloud版本 + 增加Gateway demo

---
+++ gateway/src/main/java/com/github/lj128/spring/cloud/demo/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package com.github.lj128.spring.cloud.demo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by liujia on  22:06
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
--- zuul/src/main/java/com/github/lj128/spring/cloud/demo/gateway/ZuulApplication.java
+++ zuul/src/main/java/com/github/lj128/spring/cloud/demo/gateway/ZuulApplication.java
@@ -1,4 +1,4 @@
-package com.github.lj128.spring.cloud.demo.zuul;
+package com.github.lj128.spring.cloud.demo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
