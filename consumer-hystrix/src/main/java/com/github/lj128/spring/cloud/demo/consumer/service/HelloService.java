0eb726a0f5980d0c485c4c3cb0a4bebeff8404f0
liujia <liujia@imdada.cn>
2021-08-04 10:21:40 +0800

    新增熔断配置

--- consumer-hystrix/src/main/java/com/github/lj128/spring/cloud/demo/consumer/service/HelloService.java
+++ consumer-hystrix/src/main/java/com/github/lj128/spring/cloud/demo/consumer/service/HelloService.java
@@ -1,11 +1,29 @@
 package com.github.lj128.spring.cloud.demo.consumer.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.hystrix.contrib.javanica.conf.HystrixPropertiesManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@DefaultProperties(
+        defaultFallback = "helloFallback",
+        commandProperties = {
+                @HystrixProperty(
+                        name = HystrixPropertiesManager.EXECUTION_ISOLATION_THREAD_TIMEOUT_IN_MILLISECONDS,
+                        value = "1000"),
+                @HystrixProperty(
+                        name = HystrixPropertiesManager.CIRCUIT_BREAKER_ENABLED,
+                        value = "true")
+        },
+        threadPoolProperties = {
+                @HystrixProperty(
+                        name = HystrixPropertiesManager.CORE_SIZE,
+                        value = "30")
+        })
 @Service
 @Slf4j
 public class HelloService {
@@ -18,7 +36,7 @@ public class HelloService {
 //        return restTemplate.getForObject("http://producer/hello", String.class);
         while (true) {
             //熔断并不会中断正在处理的线程
-            System.out.println("i am running ["+Thread.currentThread().getId()+"]");
+            System.out.println("i am running [" + Thread.currentThread().getId() + "]");
         }
 //        return "Hystrix test";
     }
