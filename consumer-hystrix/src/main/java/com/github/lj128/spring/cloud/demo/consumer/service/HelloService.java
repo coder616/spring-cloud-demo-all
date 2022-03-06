9451122bfed085bc5689d954b7a5ffe9e5767ee7
coderj <liu1281193637@gmail.com>
2022-03-06 11:21:40 +0800

    gateway

--- consumer-hystrix/src/main/java/com/github/lj128/spring/cloud/demo/consumer/service/HelloService.java
+++ consumer-hystrix/src/main/java/com/github/lj128/spring/cloud/demo/consumer/service/HelloService.java
@@ -1,29 +1,12 @@
 package com.github.lj128.spring.cloud.demo.consumer.service;
 
-import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-import com.netflix.hystrix.contrib.javanica.conf.HystrixPropertiesManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@DefaultProperties(
-        defaultFallback = "helloFallback",
-        commandProperties = {
-                @HystrixProperty(
-                        name = HystrixPropertiesManager.EXECUTION_ISOLATION_THREAD_TIMEOUT_IN_MILLISECONDS,
-                        value = "1000"),
-                @HystrixProperty(
-                        name = HystrixPropertiesManager.CIRCUIT_BREAKER_ENABLED,
-                        value = "true")
-        },
-        threadPoolProperties = {
-                @HystrixProperty(
-                        name = HystrixPropertiesManager.CORE_SIZE,
-                        value = "30")
-        })
 @Service
 @Slf4j
 public class HelloService {
@@ -31,14 +14,29 @@ public class HelloService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "helloFallback")
+    @HystrixCommand(groupKey = "hello", commandKey = "hello", threadPoolKey = "hello",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000"),
+                    //控制实际处理请求超时中断
+                    @HystrixProperty(name = "execution.isolation.thread.interruptOnTimeout", value = "true")
+            },
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "30"),
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "180000")
+            },
+            fallbackMethod = "helloFallback"
+    )
     public String hello() {
 //        return restTemplate.getForObject("http://producer/hello", String.class);
-        while (true) {
-            //熔断并不会中断正在处理的线程
-            System.out.println("i am running [" + Thread.currentThread().getId() + "]");
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("i am running [" + Thread.currentThread().getId() + "]");
+            }
+            System.out.println("interrupted [" + Thread.currentThread().getId() + "]");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-//        return "Hystrix test";
+        return "Hystrix test";
     }
 
     public String helloFallback() {
